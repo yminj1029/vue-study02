ed3051320ec4993da3bd845a183a40d5bb7efc1c
MinjiYun <minjiyun204@gmail.com>
2022-01-09 00:06:22 +0900

    220109 todo ui

---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+      <ul>
+          <li v-for="(todoItem,index) in todoItems" v-bind:key="todoItem" class="shadow">
+              {{todoItem}}
+              <span class="remobeBtn" v-on:click="removeTodo(todoItem, index)">
+                  <i class="fas fa-trash"></i>
+              </span>
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    data:function(){
+        return {
+            todoItems: []
+        }
+    },
+    methods:{
+        removeTodo:function(todoItem, index){
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index, 1); //기존 배열 삭제
+        }
+    },
+    created:function(){
+        if(localStorage.length >0){
+            for(var i =0; i<localStorage.length; i++){
+                if(localStorage.key(i)!=='loglevel:webpack-dev-server'){
+                    this.todoItems.push(localStorage.key(i))
+                }
+            }
+        }
+    }
+}
+</script>
+
+<style>
+ul{
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
+}
+
+li{
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+}
+.remobeBtn{
+    margin-left: auto;
+    color: #de4343;
+}
+.checkBtn{
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
+}
+.checkBtnCompleted{
+    color:#b3adad;
+}
+</style>
