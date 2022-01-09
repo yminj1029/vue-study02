976b73386dccf3390b1a02f573bf47b97d6f753d
MinjiYun <minjiyun204@gmail.com>
2022-01-09 22:23:39 +0900

    220109 section3~4 애플리케이션 리팩토링

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,48 +1,30 @@
 <template>
   <div>
-      <ul>
-          <li v-for="(todoItem,index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+    <transition-group name="list" tag="ul">
+          <li v-for="(todoItem,index) in propsdata" v-bind:key="todoItem.item" class="shadow">
             <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted:todoItem.completed}" 
-            v-on:click="toggleComplete(todoItem)"></i>
+            v-on:click="toggleComplete(todoItem,index)"></i>
             <span v-bind:class="{textCompleted:todoItem.completed}">{{todoItem.item}}</span>
               <span class="remobeBtn" v-on:click="removeTodo(todoItem, index)">
                   <i class="fas fa-trash"></i>
               </span>
         </li>
-      </ul>
+    </transition-group>
   </div>
 </template>
 
 <script>
 export default {
-    data:function(){
-        return {
-            todoItems: []
-        }
-    },
+    props:['propsdata'],
     methods:{
         removeTodo:function(todoItem, index){
-            localStorage.removeItem(todoItem);
-            this.todoItems.splice(index, 1); //기존 배열 삭제
+            this.$emit('removeItem',todoItem, index)
         },
-        toggleComplete:function(todoItem){
-            todoItem.completed = !todoItem.completed;
-            localStorage.removeItem(todoItem.item);
-            localStorage.setItem(todoItem.item, JSON.stringify(todoItem))
+        toggleComplete:function(todoItem, index){
+            this.$emit('toggleItem',todoItem,index)
         }
     },
-    created:function(){
-        if(localStorage.length >0){
-            for(var i =0; i<localStorage.length; i++){
-                if(localStorage.key(i)!=='loglevel:webpack-dev-server'){
-                    this.todoItems.push(
-                        JSON.parse(localStorage.getItem(localStorage.key(i)))
-                    );
-                    // this.todoItems.push(localStorage.key(i))
-                }
-            }
-        }
-    }
+
 }
 </script>
 
@@ -79,5 +61,12 @@ li{
 .textCompleted{
     text-decoration: line-through;
     color: #b3adad;
+}
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
