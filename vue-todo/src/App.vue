2946bc108ddc680c276a138e429bd0d652c56161
MinjiYun <minjiyun204@gmail.com>
2022-01-12 23:27:47 +0900

    220112 vuex store

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -39,18 +39,7 @@ export default {
       this.todoItems=[];
     }
   },
-  created(){
-    if(localStorage.length >0){
-        for(let i =0; i<localStorage.length; i++){
-            if(localStorage.key(i)!=='loglevel:webpack-dev-server'){
-                this.todoItems.push(
-                    JSON.parse(localStorage.getItem(localStorage.key(i)))
-                );
-                // this.todoItems.push(localStorage.key(i))
-            }
-          }
-      }
-  },
+
   components:{
     TodoHeader,
     TodoInput,
